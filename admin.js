17d24feafdd713177c7a32919bf6ad74bb2f0c2a
asherE7 <ashermorse13@gmail.com>
2026-04-03 12:15:11 -0400

    add progress bar

--- admin.js
+++ admin.js
@@ -1,4 +1,5 @@
 function uploadVideo() {
+
     const password = document.getElementById("adminPassword").value;
     const file = document.getElementById("videoFile").files[0];
 
@@ -11,12 +12,77 @@ function uploadVideo() {
     formData.append("password", password);
     formData.append("video", file);
 
-    fetch("upload.php", {
-        method: "POST",
-        body: formData
-    })
-        .then(res => res.text())
-        .then(msg => {
-            alert(msg);
-        });
+    const xhr = new XMLHttpRequest();
+
+    xhr.upload.addEventListener("progress", function (e) {
+
+        if (e.lengthComputable) {
+
+            const percent = Math.round((e.loaded / e.total) * 100);
+
+            document.getElementById("progressBar").style.width = percent + "%";
+
+        }
+
+    });
+
+    xhr.onload = function () {
+
+        alert(xhr.responseText);
+
+        document.getElementById("progressBar").style.width = "0%";
+
+    };
+
+    xhr.open("POST", "upload.php");
+
+    xhr.send(formData);
+
 }
+
+function loadCodes() {
+
+    fetch("manage_codes.php?action=list")
+        .then(res => res.json())
+        .then(codes => {
+
+            const list = document.getElementById("codeList");
+
+            list.innerHTML = "";
+
+            codes.forEach(code => {
+
+                const li = document.createElement("li");
+
+                li.innerHTML = code +
+                    " <button onclick=\"deleteCode('" + code + "')\">Delete</button>";
+
+                list.appendChild(li);
+
+            });
+
+        });
+
+}
+
+function addCode() {
+
+    const code = document.getElementById("newCode").value;
+
+    fetch("manage_codes.php?action=add", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+
+        body: "code=" + encodeURIComponent(code)
+
+    })
+        .then(() => loadCodes());
+
+}
+
+function deleteCode(code) {
+    window.onload = loadCodes;
